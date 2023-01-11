5a4d4d27fecff7d7a5426a7bedadfc8dfd5ab2e5
Eduardo Jesus do Nascimento <61081961+edulxrd@users.noreply.github.com>
2023-01-11 02:50:41 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+#%%
+import datetime
+from sched import scheduler
+import time
+
+from schedule import repeat, every, run_pending
+from ingestors import DaySummaryIngestor
+from writers import DataWriter
+
+
+if __name__ == "__main__":
+    day_summary_ingestor = DaySummaryIngestor(
+        writer=DataWriter,
+        coins=["BTC", "ETH", "LTC", "BCH"],
+        defaultstartdate=datetime.date(2021, 6, 1)
+    )
+
+
+    @repeat(every(1).seconds)
+    def job():
+        day_summary_ingestor.ingest()
+
+
+    while True:
+        run_pending()
+        time.sleep(0.5)
+
+
+
+# %%
